110c922a955bc03b526c7ee1f9bafb64d845960a
Filipe Cezar <cezarfilipe01@gmail.com>
2025-11-27 22:09:26 -0300

    revisando e criando

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.get('/',(req,res)=>{
 app.get('/cap12',(req,res)=>{
     res.send('<h2>Capítulo 12: Introdução ao Express</h2>'); 
 })
+app.use(express.json()); 
+app.post('/filmes',(req,res)=>{
+    //const titulo = req.body.titulo 
+    // const genero = req.body.genero 
+    const{titulo,genero} = req.body; 
+    res.send(`Filme: ${titulo} - Gênero: ${genero},recebido...`); 
+})
 app.listen(port,()=>{
 console.log(`Servidor rodando em http://localhost:${port}`); 
 });
