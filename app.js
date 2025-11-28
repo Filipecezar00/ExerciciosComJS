ff37dbd1c7dd7a42fb884faa44744fa1c7f869cd
Filipe Cezar <cezarfilipe01@gmail.com>
2025-11-27 21:54:31 -0300

    Aprendendo

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3001;
 app.get('/',(req,res)=>{
     res.send('olá... Bem vindo!'); 
 }); 
+app.get('/cap12',(req,res)=>{
+    res.send('<h2>Capítulo 12: Introdução ao Express</h2>'); 
+})
 app.listen(port,()=>{
 console.log(`Servidor rodando em http://localhost:${port}`); 
 });
