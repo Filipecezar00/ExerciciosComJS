eda205b02927ce7206fa22f39a568e5e7fc674d6
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-03 10:51:06 -0300

    atualizando

--- knexfile.js
+++ knexfile.js
@@ -4,50 +4,17 @@
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './data/editora.db3'
-    }
-  },
-  useNullAsDefault:true, 
-  migrations:{
-    directory: './data/migrations'
-  }, 
-  seeds:{
-    directory:  './data/seeds'
-  }, 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+  development:{
+    client: 'sqlite3', 
+    connection:{
+      filename:'./data/editora.db3'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault:true, 
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
+    }, 
+    seeds:{
+      directory:'./data/seeds'
     }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+  }, 
+}
