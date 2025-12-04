1a9f9a66bbce1f2e22bb325e5921f1caef9d0b8e
Filipe Cezar <cezarfilipe01@gmail.com>
2025-12-03 22:36:31 -0300

    atualizando

--- migrations/20251130004340_create_livros.js
+++ migrations/20251130004340_create_livros.js
@@ -7,19 +7,16 @@ exports.up = function(knex) {
     table.increments(); 
     table.string("titulo",80).notNullable();
     table.string("autor",60).notNullable(); 
-    table.integer("ano",4).notNullable(); 
-    table.decimal("preco",9.2).notNullable(); 
+    table.integer("ano").notNullable(); 
+    table.decimal("preco",9,2).notNullable(); 
     table.string("foto",100).notNullable();  
   });
 };
-    exports.down = function(knex){
-    return knex.schema.dropTable('livros'); 
-  }
-
+    
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("livros"); 
 };
