90996b6101b624f46655be425be67337f19a68d5
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-06 22:30:19 -0300

    atualizando

--- node_exercicios/index.js
+++ node_exercicios/index.js
@@ -1,16 +1,15 @@
 const express = require("express")
 const app = express()
 const rota = require("./controllers/clima")
-const porta = process.env.PORT 
+const porta = process.env.PORT || 3000 
 
 app.get("/",(req,res)=>{
     res.send("API de Clima"); 
 }); 
-app.get("/previsao/cidade/:nome",(req,res)=>{
-    res.send(req.params.nome)
-})
-app.get("(.*)",(req,res)=>{
-    res.status(404).send("Erro ao Acessar a Rota") 
+app.use("/",rota); 
+
+app.use((req,res)=>{
+    res.status(404).send("Erro ao acessar a rota")
 })
 app.listen(porta || 3000, ()=>{
     console.log("Servidor Rodando")
